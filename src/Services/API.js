13a53afb08a5a3ba89ab0cc32c368a15b1fac641
Afolabiyinka <163763149+Afolabiyinka@users.noreply.github.com>
2025-03-30 19:03:04 +0100

    Changin the api request to day instead of trending

--- src/Services/API.js
+++ src/Services/API.js
@@ -2,7 +2,9 @@ const API_KEY = "77a443f6655b62e7dfaf1f26d5ea79d6";
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getLatestMovies = async () => {
-  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const response = await fetch(
+    `${BASE_URL}/trending/all/day?api_key=${API_KEY}`
+  );
   const data = await response.json();
   return data.results;
 };
