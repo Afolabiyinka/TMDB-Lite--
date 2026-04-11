0259ba64ab7b5fd59cbe0f4ebb3661dcf67568cd
Ice Chain <afolabiyinka738@gmail.com>
2026-04-11 18:58:31 +0100

    Merge branch 'main' of https://github.com/Afolabiyinka/TMDB-Lite--

--- src/app/constants/api-data.ts
+++ src/app/constants/api-data.ts
@@ -1,5 +1,5 @@
 const testingEndpoint = "http://localhost:5050/";
-const prodEndpoint = "https://tmdb-lite-backend.onrender.com"
+const prodEndpoint = "https://tmdb-lite-backend.onrender.com/"
 const BASE_URL = "https://api.themoviedb.org/3/"
 
 export { testingEndpoint, prodEndpoint, BASE_URL };
