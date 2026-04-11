c94455d2f15843339497e5eb57adb47d4c16fc1a
Ice Chain <afolabiyinka738@gmail.com>
2026-04-11 18:42:49 +0100

    feat: enhance favourites page with scrollable card row and improve loading states and change the endpoints to production endpoint

--- src/app/constants/api-data.ts
+++ src/app/constants/api-data.ts
@@ -1,4 +1,4 @@
 const testingEndpoint = "http://localhost:5050/";
-const BASE_URL = "https://api.themoviedb.org/3"
+const BASE_URL = "https://api.themoviedb.org/3/"
 
 export { testingEndpoint, BASE_URL };
